b6820231488d52cf242ad7569e7d87a3bccc0883
spherix <spherixx@outlook.com>
2026-08-26 23:53:07 -0300

    feat: lógica da calculadora

---
+++ src/swaplator/calculadora/calculadora.py
@@ -0,0 +1,76 @@
+class Calculadora:
+
+    def __init__(self):
+        self.limpar()
+
+
+    def limpar(self):
+        self.num1 = None
+        self.operador = None
+        self.entrada_atual = ""
+
+        self.novo_numero = False
+
+
+    def digitar(self, valor):
+        if(self.novo_numero):
+            self.entrada_atual = ""
+            self.novo_numero = False
+
+        if(valor == "." and self.entrada_atual == ""):
+            self.entrada_atual = "0."
+            return
+
+        if(valor == "." and "." in self.entrada_atual):
+            raise ValueError("A entrada já possui um ponto decimal")
+
+        self.entrada_atual += valor
+
+
+    def selecionar_operador(self, operador):
+        if(self.entrada_atual == ""):
+            raise ValueError("Primeiro número não informado.")
+
+        self.operador = operador
+
+        self.num1 = float(self.entrada_atual)
+        self.entrada_atual = ""
+        self.novo_numero = False
+            
+
+    def calcular(self):
+        if(self.entrada_atual == ""):
+            raise ValueError("Segundo número não informado.")
+        
+        num2 = float(self.entrada_atual)
+
+        if(self.operador == "+"):
+            resultado = self.num1 + num2
+        
+        elif(self.operador == "-"):
+            resultado = self.num1 - num2
+
+        elif(self.operador == "*" or self.operador == "x"):
+            resultado = self.num1 * num2
+
+        elif(self.operador == "/" or self.operador == ":"):
+            resultado = self.num1 / num2
+
+        self.entrada_atual = str(resultado)
+        self.novo_numero = True
+
+        
+
+calculadora = Calculadora()
+calculadora.digitar("1")
+calculadora.digitar("2")
+calculadora.selecionar_operador("+")
+calculadora.digitar("5")
+calculadora.calcular()
+
+print(calculadora.entrada_atual)
+
+calculadora.digitar("3")
+calculadora.digitar("4")
+
+print(calculadora.entrada_atual)
