62268770362ee24615313727b1d535fa6e6feb76
spherix <spherixx@outlook.com>
2026-08-31 23:04:17 -0300

    feat: tratamento de exceções para o conversor de moedas

--- src/swaplator/conversores/moedas.py
+++ src/swaplator/conversores/moedas.py
@@ -1,14 +1,44 @@
+from numbers import Number
 import requests
 
-def obter_cotacoes():
-    dados = requests.get("https://api.frankfurter.dev/v2/rates?base=USD&quotes=BRL,EUR").json()
 
-    DICIONARIO_MOEDAS = {}
+def obter_cotacoes():
+    cotacoes = {}
+
+    try:
+        resposta = requests.get("https://api.frankfurter.dev/v2/rates?base=USD&quotes=BRL,EUR")
+        resposta.raise_for_status()
+
+    except requests.HTTPError as e:
+        raise ValueError("Um erro HTTP foi detectado.") from e
+
+    except requests.RequestException as e:
+        raise ValueError("Não foi possível conectar à API.") from e    
+
+    dados = resposta.json()
+
+    if not isinstance(dados, list):
+        raise ValueError("Formato de resposta inesperado: 'DADOS' não é uma 'LISTA'.")
 
     for dado in dados:
-        DICIONARIO_MOEDAS[dado["quote"]] = dado["rate"]
+        if not isinstance(dado, dict):
+            raise ValueError("Formato de resposta inesperado: 'DADO' não é um 'DICT'.")
+        
+        if "quote" not in dado:
+            raise ValueError("Formato de resposta inesperado: 'QUOTE' não está presente no dicionário.")
+        
+        if not isinstance(dado["quote"], str):
+            raise ValueError("Formato de resposta inesperado: 'QUOTE' não é uma 'STRING'.")
+        
+        if "rate" not in dado:
+            raise ValueError("Formato de resposta inesperado: 'RATE' não está presente no dicionário.")
+        
+        if not isinstance(dado["rate"], Number):
+            raise ValueError("Formato de resposta inesperado: 'RATE' não é um 'NÚMERO'.")
 
-    return DICIONARIO_MOEDAS
+        cotacoes[dado["quote"]] = dado["rate"]
+
+    return cotacoes
 
 
 def converter_moedas(valor, moeda_inicial, moeda_final, cotacoes):
